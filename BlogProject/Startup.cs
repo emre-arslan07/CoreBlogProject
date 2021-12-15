1bbff7effc9afe9ae72858c65b0e8a3a26be7308
EMRE ARSLAN <emrearslan_ynne@hotmail.com>
2021-12-15 19:04:26 +0300

    Admin panel ve area eklendş

--- BlogProject/Startup.cs
+++ BlogProject/Startup.cs
@@ -84,6 +84,11 @@ namespace BlogProject
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                   name: "areas",
+                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+               );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
